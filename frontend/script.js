830d455e8fd1d4e0006075bdff8e0ea870513178
bipulshakya <154542673+bipulshakya@users.noreply.github.com>
2025-01-31 14:32:04 +0545

    modified some animations

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,25 @@
 const crsr = document.querySelector("#cursor")
 const blur = document.querySelector("#cursor-blur")
 document.addEventListener("mousemove",function(dets){
-  crsr.style.left = dets.x+"px"
-  crsr.style.top = dets.y+"px"
+  crsr.style.left = dets.x+  "px"
+  crsr.style.top = dets.y+ "px"
   blur.style.left = dets.x-200+"px"
   blur.style.top = dets.y-200+"px"
 })
+
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid rgba(255, 13, 0, 0.793)";
+    crsr.style.backgroundColor = "rgba(255, 13, 0, 0.793)";
+  });
+});
 
 // Register the ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
@@ -14,7 +28,7 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.to("#nav", {
   duration: 0.5,
   height: "130px",
-  backgroundColor: " black", // Changes background color to black
+  background: " linear-gradient(to left, red, blue)", // Changes background color to black
   scrollTrigger: {
     trigger: "#nav", // The element that triggers the animation
     scroller: "body", // The scroller (usually "body" unless you have a custom container)
@@ -109,17 +123,55 @@ gsap.from("#school-gallery", {
   },
 });
 
-new Swiper('.card-wrapper', {
-  loop: true,
+// new Swiper('.card-wrapper', {
+  //loop: true, //
 
   // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+ // pagination: {
+   // el: '.swiper-pagination',
+  //}, 
 
   // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+ // navigation: {
+  //  nextEl: '.swiper-button-next',
+//prevEl: '.swiper-button-prev',
+  //},
+//}); 
+
+gsap.from("#colon1", {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+
+gsap.from("#colon2", {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+
+gsap.from("#page4 h1", {
+  y: 50,
+  scrollTrigger: {
+    trigger: "#page4 h1",
+    scroller: "body",
+    // markers:true,
+    start: "top 75%",
+    end: "top 70%",
+    scrub: 3,
   },
 });
